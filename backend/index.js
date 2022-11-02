942ab604b99ac9961a92ec6b6672d6ec2db74e9b
tsdenouden <trisdo687@gmail.com>
2022-11-02 22:09:46 +0100

    basic endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 // initialise express app
 const app = express()
 
+app.use(express.json())
+
 let db_studysets=[
     {
         id: 1,
@@ -29,9 +31,33 @@ let db_studysets=[
     },
 ]
 
-app.get('/', (request, response) => {
+app.get('/api/studysets', (request, response) => {
     response.json(db_studysets)
 })
+
+app.get('/api/studysets/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const findStudySet = db_studysets.filter(set => set.id === id)
+    response.json(findStudySet)
+})
+
+app.post('/api/studysets', (request, response) => {
+    const body = request.body
+    db_studysets = db_studysets.concat(body)
+    response.send(body)
+})
+
+app.delete('/api/studysets/:id', (request, response) => {
+    const id = Number(request.params.id)
+    db_studysets = db_studysets.filter(set => set.id !== id)
+    response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
